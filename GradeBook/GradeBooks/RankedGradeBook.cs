2d893ff661f15942201cbcd3024ad4ff57abdb11
adrian <adrian@perdomo.dk>
2019-03-27 14:09:06 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -23,8 +23,8 @@ namespace GradeBook.GradeBooks
                 return 'A';
             else if (grades[(threshold * 2) - 1] <= averageGrade)
                 return 'B';
-            else if (grades[(threshold*3)-1] <= averageGrade)
-
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
             return base.GetLetterGrade(averageGrade);
         }
     }
